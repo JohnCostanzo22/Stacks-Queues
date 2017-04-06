8a0edbbabb713b1b0fe0b3917cc93da919088025
JohnCostanzo22 <jacostanzo@albany.edu>
2017-04-06 15:00:31 -0400

    Added comments to Node class

--- src/Node.java
+++ src/Node.java
@@ -1,36 +1,40 @@
+/*
+ * Class that creates objects called Nodes That contain strings
+ * and keeps track of the previous Node
+ */
 public class Node {
 	
+	//properties
 	private String name = "";
-	private Node next;
 	private Node previous;
 	
+	/**
+	 * Constructor that sets name field and sets 
+	 * @param name - String contained in the Node
+	 */
 	public Node(String name) {
 		this.name = name;
-		this.next = null;
 		this.previous = null;
 	}
-	
-	public Node getNext() {
-		return next;
-	}
-
+	/**
+	 * Mutator method that changes the previous node
+	 * @param node - previous node
+	 */
 	public void setPrevious(Node node) {
 		this.previous = node;
 	}
-
+	/**
+	 * Accessor method
+	 * @return The previous Node
+	 */
 	public Node getPrevious() {
 		return previous;
 	}
-
-	public void setNext(Node node) {
-		this.next = node;
-	}
-
+	/**
+	 * Accessor method
+	 * @return the Name of the Node
+	 */
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 }
