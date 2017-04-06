a14dade40c46eaaeb14ab697fb12364634c1ba48
JohnCostanzo22 <jacostanzo@albany.edu>
2017-04-06 15:05:50 -0400

    Added comments to Stack class

--- src/Stack.java
+++ src/Stack.java
@@ -1,14 +1,29 @@
+/**
+ * Class that creates a Stack of Nodes
+ *
+ */
 public class Stack {
 
+	//properties
 	private Node tail;
 	private Node placeHolder;
 	
+	/**
+	 * Default contructor
+	 */
 	public Stack() {
 		tail = null;
 		placeHolder = null;
 	}
 	
+	/**
+	 * Adds a new Node to the top of the stack
+	 * @param newNode - Node to be added
+	 */
 	public void push(Node newNode) {
+		if(newNode == null) {
+			throw new IllegalArgumentException("The Node is empty");
+		}
 		if(tail == null) {
 			tail = newNode;
 		}
@@ -18,23 +33,31 @@ public class Stack {
 		}
 	}
 	
+	/**
+	 * Gets the top Node, removes it from the stack and returns the Node
+	 * @return - Top Node
+	 */
 	public Node pop() {
 		if(tail == null) {
-			//System.out.println("The stack is empty");
 			throw new IllegalArgumentException("The Stack is empty");
 		}
 		else if(tail.getPrevious() == null) {
 			placeHolder = tail;
 			tail = null;
-			placeHolder.setPrevious(null);   				//reset the node so it can be reused
+			//reset the node so it can be reused
+			placeHolder.setPrevious(null);  
 			return placeHolder;
 		}
 		placeHolder = tail;
 		tail = tail.getPrevious();
-		placeHolder.setPrevious(null);   				//reset the node so it can be reused
+		//reset the node so it can be reused
+		placeHolder.setPrevious(null);  
 		return placeHolder;
 	}
-	
+	/**
+	 * Return the top Node but does not delete it
+	 * @return - Top Node
+	 */
 	public Node peek() {
 		if(tail == null) {
 			throw new IllegalArgumentException("The Stack is empty");
